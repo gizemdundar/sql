cb7987eba477b472209bc58a8b4a67ce750d7f1c
gizemdundar <110562100+gizemdundar@users.noreply.github.com>
2022-08-19 10:36:45 +0300

    Create odev3.sql
    patika.dev sql3

---
+++ odev3.sql
@@ -0,0 +1,14 @@
+https://www.patika.dev/
+
+--odev3
+--1.SORU
+SELECT country FROM country WHERE country LIKE 'A%a';
+
+--2.SORU
+SELECT country FROM country WHERE country LIKE '_____n';
+
+--3.SORU
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+
+--4.SORU
+SELECT * FROM film WHERE title LIKE 'C%' AND lentgth > 90 AND rental_rate = 2.99;
