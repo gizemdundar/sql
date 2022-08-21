0d5d50b1de68e4c797e83985c57625b8a7222ee0
gizemdundar <110562100+gizemdundar@users.noreply.github.com>
2022-08-21 10:18:38 +0300

    Create odev6.sql
    patika.dev sql odev6

---
+++ odev6.sql
@@ -0,0 +1,9 @@
+--odev6
+--1.SORU:
+SELECT AVG(rental_rate) FROM film;
+--2.SORU:
+SELECT COUNT(*) FROM film WHERE title = 'C%';
+--3.SORU:
+SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
+--4.SORU:
+SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150;
