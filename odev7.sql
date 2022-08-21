6c88aa1c448586c8f2eafa7c10cbd75da6ed7b62
gizemdundar <110562100+gizemdundar@users.noreply.github.com>
2022-08-21 21:12:59 +0300

    Create odev7.sql
    patika.dev sql odev7

---
+++ odev7.sql
@@ -0,0 +1,20 @@
+--odev7
+--1.SORU:
+SELECT * FROM film
+GROUP BY rating;
+
+--2.SORU: 
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50
+ORDER BY COUNT(*);
+
+--3.SORU:
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4.SORU:
+SELECT * FROM city 
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
