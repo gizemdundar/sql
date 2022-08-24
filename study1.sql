4661397c2319754f9ab1223d5c7237945cd625df
gizemdundar <110562100+gizemdundar@users.noreply.github.com>
2022-08-24 13:42:49 +0300

    Update study1.sql

--- study1.sql
+++ study1.sql
@@ -1,20 +1,20 @@
--- ödev 1 
+-- STUDY 1 
 
---1.soru
+--Q1
 SELECT title, description FROM film;
 
---2.soru
+--Q2
 SELECT * FROM film
 WHERE length > 60 AND length < 75;
 
---3.soru
+--Q3
 SELECT * FROM film
 WHERE rental_rate=0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
---4.soru
+--Q4
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
---5.soru
+--Q5
 SELECT * FROM film
 WHERE NOT (length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99));
